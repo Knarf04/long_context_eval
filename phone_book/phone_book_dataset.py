9a6cf3870cfe60857c089695376a8ef6f9463826
Haochen Shen <hshen14@dt-login01.delta.ncsa.illinois.edu>
2025-03-13 09:33:05 -0500

    Phone Book Retrieval Added

---
+++ phone_book/phone_book_dataset.py
@@ -0,0 +1,218 @@
+import random
+import textwrap
+import names
+import torch
+import argparse
+import os
+import multiprocessing as mp
+from tqdm import tqdm
+
+from torch.utils.data import Dataset
+from transformers import AutoTokenizer
+
+# Modified following: https://arxiv.org/abs/2406.07887 section 3.3.3
+class PhoneBookDataset(Dataset):
+    def __init__(self,  
+                 length: int = 1000, 
+                 tokenizer: AutoTokenizer = None, 
+                 size: int = 1000,
+                 few_shot: int = 2, 
+                 reversed: bool = False):
+        
+        assert length > 500, "The length must be greater than 500 to ensure proper depth control."
+        assert size >= 100, "The size of the data set must be larger enough to cover all percentile."
+
+        self.length = length
+        self.tokenizer = tokenizer
+        self.few_shot = few_shot
+        self.size = size
+        self.reversed = reversed
+        
+        if not self.reversed:
+            self.few_shot_template = textwrap.dedent('''\
+                What is the phone number for {name}?
+                Answer: {phone}\
+                ''')
+        else:
+            self.prefix_template = textwrap.dedent('''\
+                Given a phone book with entries of the form:
+                {few_shot_examples}
+                ... and so on.
+                Please find the phone number for {name}.
+                I will ask you for this phone number later.
+                Memorize it so you can respond correctly.
+                Remember {name}.
+                Here is the phonebook:\n
+                ''')
+    
+            self.suffix_template = textwrap.dedent('''\
+                Okay, that is the end of the phonebook.
+                Remember when I asked you to memorize the phone number for {name}?
+                What is the phone number for {name}?\
+                ''')
+
+    def _gen_phone(self):
+        # Ensure the area code and exchange don't start with 0 or 1.
+        area_code = random.randint(200, 999)
+        exchange = random.randint(200, 999)
+        subscriber = random.randint(1000, 9999)
+        return f"{area_code}-{exchange}-{subscriber}"   
+    
+    # Generate phone book with controlled insertion of few-shot examples
+    def _gen_phone_book(self, depth: int = 50):
+        # Generate names and phone numbers for few-shot samples and target.
+        name_list = []
+        phone_list = []
+        for _ in range(self.few_shot + 1):
+            name_list.append(names.get_full_name())
+            phone_list.append(self._gen_phone())
+
+        if not self.reversed:
+            suffix = []
+            for i in range(self.few_shot):
+                suffix.append(self.few_shot_template.format(name=name_list[i], phone=phone_list[i]))
+            suffix.append(self.few_shot_template.format(name=name_list[self.few_shot], phone=''))
+            prefix = ""
+            suffix = "\n".join(suffix)
+        else:
+            few_shot_examples = []
+            for i in range(self.few_shot):
+                few_shot_examples.append(f"{name_list[i]}: {phone_list[i]}")
+            few_shot_examples = "\n".join(few_shot_examples)
+            prefix = self.prefix_template.format(few_shot_examples=few_shot_examples, name=name_list[self.few_shot])
+            suffix = self.suffix_template.format(name=name_list[self.few_shot])
+        
+        # The label is the target phone number (last element).
+        label = phone_list[self.few_shot]
+        
+        if self.tokenizer:
+            prefix_ids = self.tokenizer(prefix, add_special_tokens=(self.tokenizer.bos_token is not None))["input_ids"]
+            suffix_ids = self.tokenizer(suffix, add_special_tokens=False)["input_ids"]
+            # Reserve space for prefix and suffix.
+            phone_book_count = self.length - len(prefix_ids) - len(suffix_ids)
+            input_ids = prefix_ids.copy()
+        else:
+            phone_book_count = self.length
+
+        phone_book_raw = []
+
+        # Pre-calculate positions for insertions.
+        few_shot_positions = [((j + 1) * phone_book_count) // (self.few_shot + 1) for j in range(self.few_shot)]
+        few_shot_idx = 0 
+        target_position = int((depth / 100) * phone_book_count * 0.97) #
+        target_inserted = False
+
+        i = 0
+        while i < phone_book_count:
+            # Check if we need to insert the target entry.
+            if not target_inserted and i >= target_position:
+                phone_book_entry = f"{name_list[self.few_shot]}: {phone_list[self.few_shot]}\n"
+                target_inserted = True
+            # Otherwise, check if it's time for a few-shot example.
+            elif few_shot_idx < self.few_shot and i >= few_shot_positions[few_shot_idx]:
+                phone_book_entry = f"{name_list[few_shot_idx]}: {phone_list[few_shot_idx]}\n"
+                few_shot_idx += 1
+            else:
+                # Otherwise, add a random entry.
+                phone_book_entry = f"{names.get_full_name()}: {self._gen_phone()}\n"
+
+            if self.tokenizer:
+                phone_book_ids = self.tokenizer(phone_book_entry, add_special_tokens=False)["input_ids"]
+                if i + len(phone_book_ids) > phone_book_count:
+                    break
+                i += len(phone_book_ids)
+                input_ids.extend(phone_book_ids)
+            else:
+                i += 1
+            phone_book_raw.append(phone_book_entry)
+
+
+        phone_book_raw = "".join(phone_book_raw)
+        input_raw = prefix + phone_book_raw + suffix
+        if self.tokenizer:
+            input_ids.extend(suffix_ids)
+            pad_length = phone_book_count - i
+            pad_id = self.tokenizer.pad_token_id if self.tokenizer.pad_token_id is not None else 0
+            if self.tokenizer.padding_side == "right":
+                input_ids[len(input_ids):] = [pad_id] * pad_length
+                input_ids = torch.tensor(input_ids)
+                attention_mask = torch.cat((torch.ones(self.length - pad_length), torch.zeros(pad_length)), dim=0)
+            else: 
+                input_ids[0:0] = [pad_id] * pad_length
+                input_ids = torch.tensor(input_ids)
+                attention_mask = torch.cat((torch.zeros(pad_length), torch.ones(self.length - pad_length)), dim=0)
+        else:
+            input_ids, attention_mask = None, None
+
+        return input_raw, input_ids, attention_mask, label
+
+    def __len__(self):
+        return self.size
+
+    def __getitem__(self, idx):
+        depth = idx % 100  # convert idx into a percentage evenly
+        input_raw, input_ids, attention_mask, label = self._gen_phone_book(depth)
+        if self.tokenizer:
+            return {
+                'input_raw': input_raw, 
+                'input_ids': input_ids, 
+                'attention_mask': attention_mask, 
+                'label': label,
+                'depth': depth
+            }
+        else:
+            return {
+                'input_raw': input_raw, 
+                'label': label,
+                'depth': depth
+            }
+
+global_dataset = None
+
+def init_worker(model_name, dataset_params):
+    global global_dataset
+
+    tokenizer = None
+    if model_name is not None:
+        tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+        tokenizer.pad_token = tokenizer.eos_token
+        tokenizer.padding_side = 'left'
+    global_dataset = PhoneBookDataset(tokenizer=tokenizer, **dataset_params)
+
+def generate_sample(idx):
+    return global_dataset[idx]
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', default=None, type=str, help="Tokenizer for length control.") 
+    parser.add_argument('--length', default=4096, type=int, help="Tokenized input sequence length.") 
+    parser.add_argument('--size', default=100, type=int, help="Number of samples in dataset.") 
+    parser.add_argument('--few-shot', default=2, type=int, help="Few-shot examples in prompt.") 
+    parser.add_argument('--reversed', action='store_true', help="Use reversed prompt template.") 
+    parser.add_argument('--save-path', required=True, type=str, help="Path to save dataset.") 
+    args = parser.parse_args()
+
+    torch.cuda.manual_seed(42)
+    torch.manual_seed(42)
+
+    dataset_params = {
+        'length': args.length,
+        'size': args.size,
+        'few_shot': args.few_shot,
+        'reversed': args.reversed
+    }
+
+    # Set up multiprocessing pool and reinitialize tokenizer in each worker.
+    with mp.Pool(processes=mp.cpu_count(),
+                 initializer=init_worker,
+                 initargs=(args.model, dataset_params)) as pool:
+        dataset_list = list(tqdm(pool.imap(generate_sample, range(args.size)),
+                                 total=args.size, desc="Generating dataset"))
+    
+    tokenized = (args.model is not None)
+    dataset_name = f"length={args.length}_tokenized={tokenized}_size={args.size}_few-shot={args.few_shot}_reversed={args.reversed}.pt"
+    
+    os.makedirs(args.save_path, exist_ok=True)
+    dataset_path = os.path.join(args.save_path, dataset_name)
+    torch.save(dataset_list, dataset_path)
+    print(f"Dataset saved to {args.save_path}")
---
+++ phone_book/phone_book_eval.py
@@ -0,0 +1,139 @@
+import os
+import argparse
+import functools
+from tqdm import tqdm
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+# from mamba_ssm.models.mixer_seq_simple import MambaLMHeadModel
+from phone_book_dataset import PhoneBookDataset
+
+import torch
+from torch.utils.data import DataLoader
+
+import torch.distributed as dist
+from torch.utils.data.distributed import DistributedSampler
+
+# FSDP2
+from torch.distributed._composable.fsdp import fully_shard, register_fsdp_forward_method
+from torch.distributed.device_mesh import init_device_mesh
+
+
+# See FSDP's problem with model.generate: 
+# https://github.com/huggingface/transformers/issues/30228#issuecomment-2350022762
+# FSDP2 turns out to be a better option
+
+def phone_book_evaluation(model, tokenizer, dataloader, rank, world_size):
+    device = torch.device(torch.distributed.get_rank())
+    batch_registers = torch.zeros(3).to(device) # [correct, total, length]
+    depth_registers = torch.zeros(100).to(device) # Registers success cases for each percentile
+
+    dataloader_pb = tqdm(dataloader, total=len(dataloader)) if rank == 0 else dataloader
+
+    for batch in dataloader_pb:
+        inputs = batch['input_ids'].to(device)
+        attention_masks = batch['attention_mask'].to(device)
+        labels = batch['label'] 
+        depths = batch['depth']
+        max_length = inputs.shape[1] + 50
+        
+        out = model.generate(
+            input_ids=inputs,
+            attention_mask=attention_masks,
+            max_length=max_length,
+            return_dict_in_generate=True,
+            pad_token_id=tokenizer.eos_token_id
+        )
+        
+        input_length = inputs.shape[1]
+        generated_tokens = out.sequences[:, input_length:]
+        decoded_preds = tokenizer.batch_decode(generated_tokens.tolist())
+        
+        batch_correct = 0
+        for pred, label, depth in zip(decoded_preds, labels, depths):
+            pred_model = pred.split("\n")[0].strip()
+            if pred_model == label:
+                batch_correct += 1
+                depth_registers[depth] += 1
+        
+        batch_registers[0] += batch_correct
+        batch_registers[1] += inputs.shape[0] 
+        # register average actual sequence length
+        batch_registers[2] += torch.sum(attention_masks).item() 
+
+    dist.all_reduce(batch_registers, op=dist.ReduceOp.SUM)
+    dist.all_reduce(depth_registers, op=dist.ReduceOp.SUM)
+
+    if rank == 0:
+        total_accuracy = batch_registers[0] / batch_registers[1]
+        avg_length = batch_registers[2] / batch_registers[1]
+        print('Average length: {:.2f} tokens, Accuracy: {}/{} ({:.2f}%)\n'.format(
+            avg_length, batch_registers[0], batch_registers[1],
+            100. * total_accuracy))
+    return batch_registers, depth_registers
+
+def main(rank, world_size, args):
+    dist.init_process_group("nccl", rank=rank, world_size=world_size, init_method="env://")
+
+    device = torch.device(torch.distributed.get_rank())
+    torch.cuda.set_device(device)
+
+    model = AutoModelForCausalLM.from_pretrained(args.model, trust_remote_code=True, device_map=device).to(device)
+    
+    mesh = init_device_mesh("cuda", (torch.distributed.get_world_size(),))
+
+    for layer in model.model.layers:
+        fully_shard(layer, mesh=mesh)
+    fully_shard(model, mesh=mesh)
+
+    register_fsdp_forward_method(model, "generate")
+    model.eval()
+
+    tokenizer = AutoTokenizer.from_pretrained(args.model, token=hf_token, trust_remote_code=True)
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = 'left'
+
+    for length in map(int, args.length_list.split(',')):
+        print(f"Begin testing on phone books of length {length}...")
+        dataset_name = f"length={length}_tokenized=True_size={args.size}_few-shot={args.few_shot}_reversed={args.reversed}.pt"
+        dataset_path = os.path.join(args.save_path, dataset_name)
+
+        if os.path.exists(dataset_path):
+            print(f"Load dataset from {dataset_path}")
+            phonebook = torch.load(dataset_path)
+        else:
+            print(f"No dataset found at {dataset_path}. Generating a new dataset.")
+            phonebook = PhoneBookDataset(
+                length=length, 
+                tokenizer=tokenizer, 
+                size=args.size,
+                few_shot=args.few_shot,
+                reversed=args.reversed
+            )
+
+        sampler = DistributedSampler(phonebook, rank=rank, num_replicas=world_size, shuffle=True)
+        dataloader = DataLoader(phonebook, batch_size=args.batch_size, sampler=sampler, num_workers=1)
+
+        batch_registers, depth_registers = phone_book_evaluation(model,tokenizer,dataloader,rank,world_size)
+        if rank == 0:
+            print(batch_registers, depth_registers)
+        torch.distributed.barrier()
+
+    dist.destroy_process_group()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, required=True)
+    parser.add_argument('--length-list', default='4096,8192,16384', type=str, help="List of tokenized input sequence length.") 
+    parser.add_argument('--batch-size', default=32, type=int, help="Batch size of the input.") 
+    parser.add_argument('--size', default=100, type=int, help="Number of samples in dataset.") 
+    parser.add_argument('--few-shot', default=2, type=int, help="Few-shot examples in prompt.") 
+    parser.add_argument('--reversed', action='store_true', help="Use reversed prompt template.")
+    parser.add_argument('--save-path', type=str, help="Path to save dataset.")  
+    args = parser.parse_args()
+
+    torch.cuda.manual_seed(42)
+    torch.manual_seed(42)
+
+    rank = int(os.environ.get("RANK", 0))
+    world_size = int(os.environ.get("WORLD_SIZE", torch.cuda.device_count()))
+    main(rank, world_size, args)
